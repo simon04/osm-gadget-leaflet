ddeaecfe8c00363e1a2a3f0998e2b912809b031f
Simon Legner <Simon.Legner@gmail.com>
2015-11-18 21:23:09 +0100

    Add .eslintrc and fix issues

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Create a map
 var map = L.map('map').setView([40.75, -73.96], 4);
 
@@ -8,31 +10,32 @@ wikimediaLayer().addTo(map);
 L.control.scale().addTo(map);
 
 // Update the zoom level label
-map.on('zoomend', function () {
+map.on('zoomend', function() {
 });
 
 function wikimediaLayer() {
   // Allow user to change style via the ?s=xxx URL parameter
   // Uses "osm-intl" as the default style
-  var match = location.search.match(/s=([^&\/]*)/);
+  var match = window.location.search.match(/s=([^&\/]*)/);
   var style = (match && match[1]) || 'osm-intl';
   var scale = bracketDevicePixelRatio();
   var scalex = (scale === 1) ? '' : ('@' + scale + 'x');
   return L.tileLayer('https://maps.wikimedia.org/' + style + '/{z}/{x}/{y}' + scalex + '.png', {
-      maxZoom: 18,
-      attribution: 'Wikimedia maps beta | Map data &copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-      id: 'wikipedia-map-01'
+    maxZoom: 18,
+    attribution: 'Wikimedia maps beta | Map data &copy; ' +
+        '<a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+    id: 'wikipedia-map-01'
   });
 }
 
 function bracketDevicePixelRatio() {
-    var brackets = [1, 1.3, 1.5, 2, 2.6, 3],
-        baseRatio = window.devicePixelRatio || 1;
-    for (var i = 0; i < brackets.length; i++) {
-        var scale = brackets[i];
-        if (scale >= baseRatio || (baseRatio - scale) < 0.1) {
-            return scale;
-        }
+  var brackets = [1, 1.3, 1.5, 2, 2.6, 3];
+  var baseRatio = window.devicePixelRatio || 1;
+  for (var i = 0; i < brackets.length; i++) {
+    var scale = brackets[i];
+    if (scale >= baseRatio || (baseRatio - scale) < 0.1) {
+      return scale;
     }
-    return brackets[brackets.length - 1];
+  }
+  return brackets[brackets.length - 1];
 }
