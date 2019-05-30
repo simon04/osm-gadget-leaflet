39609985bff99126141e6701f571e946fbe7225a
Simon Legner <Simon.Legner@gmail.com>
2019-05-30 15:00:09 +0200

    Drop leaflet-tilelayer-geojson

--- src/layer.mediawiki.js
+++ src/layer.mediawiki.js
@@ -1,8 +1,7 @@
 import L from 'leaflet';
-import 'leaflet-tilelayer-geojson';
 import getFilePath from 'wikimedia-commons-file-path/build/wikimedia-commons-file-path';
 
-var xx = L.GeoJSON.extend({
+var renderer = L.GeoJSON.extend({
   initialize: function(options) {
     options = options || {};
     options.pointToLayer = this.pointToLayer.bind(this);
@@ -61,72 +60,10 @@ var xx = L.GeoJSON.extend({
       }
       return html;
     }
-  },
-
-  updateMarks: function() {
-    if (!this._map) {
-      return;
-    }
-    var bounds = this._map.getBounds();
-    var url = this.options.url + '/w/api.php';
-    url += L.Util.getParamString({
-      origin: '*',
-      format: 'json',
-      action: 'query',
-      list: 'geosearch',
-      gsnamespace: this.options.gsnamespace,
-      gslimit: 500,
-      gsprop: 'type|name',
-      gsbbox: [
-        bounds.getNorth(),
-        bounds.getWest(),
-        bounds.getSouth(),
-        bounds.getEast()
-      ].join('|')
-    });
-
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', updateLayer.bind(this));
-    xhr.open('GET', url);
-    xhr.send();
-    return this;
-
-    function updateLayer() {
-      if (xhr.status !== 200 || !xhr.responseText) {
-        return;
-      }
-      var json = JSON.parse(xhr.responseText);
-      if (json.error || !json.query.geosearch) {
-        console.warn(json.error);
-        return;
-      }
-      var geojson = json.query.geosearch.map(toFeature, this);
-      this.clearLayers();
-      this.addData(geojson);
-    }
-
-    function toFeature(object) {
-      var thumbnail = object.title.match(/^File:/, '')
-        ? getFilePath(object.title, this.options.thumbnailWidth)
-        : undefined;
-      return {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [object.lon, object.lat]
-        },
-        properties: {
-          title: object.title,
-          wikipediaUrl: this.options.url + '/wiki/' + object.title,
-          thumbnailWidth: this.options.thumbnailWidth,
-          thumbnail: thumbnail
-        }
-      };
-    }
   }
 });
 
-export default L.TileLayer.GeoJSON.extend({
+export default L.TileLayer.extend({
   options: {
     url: undefined,
     gsnamespace: 0,
@@ -134,15 +71,30 @@ export default L.TileLayer.GeoJSON.extend({
     thumbnailWidth: 300
   },
   initialize: function(options) {
+    this.renderingLayer = new L.GeoJSON();
     L.TileLayer.GeoJSON.prototype.initialize.call(this, undefined, options);
   },
-  _loadTile: function(tile, tilePoint) {
-    var layer = this;
-    var req = new XMLHttpRequest();
-    this._requests.push(req);
-    req.onreadystatechange = this._xhrHandler(req, layer, tile, tilePoint);
-    req.open('GET', this.getTileUrl(tilePoint), true);
-    req.send();
+  onAdd: function(map) {
+    L.TileLayer.prototype.onAdd.call(this, map);
+    map.addLayer(this.renderingLayer);
+  },
+  onRemove: function(map) {
+    map.removeLayer(this.renderingLayer);
+    L.TileLayer.prototype.onRemove.call(this, map);
+  },
+  createTile: function(coords, done) {
+    var url = this.getTileUrl(coords);
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(this._tileDataToGeoJSON.bind(this))
+      .then(
+        function(features) {
+          this.renderingLayer.addData(features);
+          done(null, document.createElement('div'));
+        }.bind(this)
+      );
   },
   getTileUrl: function(coords) {
     var bounds = this._tileCoordsToBounds(coords);
@@ -163,12 +115,6 @@ export default L.TileLayer.GeoJSON.extend({
       ].join('|')
     });
     return url;
-  },
-  _tileLoaded: function(tile, tilePoint) {
-    if (!tile.datum) return;
-    var geoJSON = this._tileDataToGeoJSON(tile.datum);
-    console.log(geoJSON)
-    this.addTileData(geoJSON, tilePoint);
   },
   _tileDataToGeoJSON: function(json) {
     return json.query.geosearch.map(function toFeature(object) {
